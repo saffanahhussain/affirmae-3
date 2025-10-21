d3b7c7b3b0bc182c89938de44d46cce51cf4f0b0
saffanah.g.hussain@student.uts.edu.au <saffanah.g.hussain@student.uts.edu.au>
2025-10-21 04:21:04 +0000

    Created fairy_wing.png, fairy_body.png and fairy_outfit.png; Edited CreateAvatar

--- client_code/CreateAvatar/__init__.py
+++ client_code/CreateAvatar/__init__.py
@@ -12,3 +12,21 @@ class CreateAvatar(CreateAvatarTemplate):
   def form_show(self, **event_args):
     """This method is called when the form is shown on the page"""
     pass
+
+  def draw_fairy(self):
+    canvas = self.fairy_canvas
+    canvas.clear()
+
+    self.Fairy_Wing.items = [
+    ("Blue Wings", "Fairy Wing") 
+
+      
+
+# Draw it on the canvas
+self.fairy_body_canvas.draw_image("theme:fairy_body.png", 50, 50)
+
+# Draw wings
+    canvas.draw_image(f"/_/theme/{self.wing_dropdown.selected_value}.png", 50, 50)
+
+# Draw outfit
+    canvas.draw_image(f"/_/theme/{self.outfit_dropdown.selected_value}.png", 50, 50)
